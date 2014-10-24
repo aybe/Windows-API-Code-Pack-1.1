4b0ec69c792115dea16d80548aaa2692ce2860f2
Aybe <aybe.one@hotmail.com>
2014-10-24 20:05:21 +0200

    Updated packages + DirectX package

--- .nuget/build.bat
+++ .nuget/build.bat
@@ -2,8 +2,11 @@
 cls
 call clean.bat
 echo Building NuGet packages ...
-nuget pack WindowsAPICodePack-Core.1.1.nuspec -NoPackageAnalysis
-nuget pack WindowsAPICodePack-ExtendedLinguisticServices.1.1.nuspec -NoPackageAnalysis
-nuget pack WindowsAPICodePack-Sensors.1.1.nuspec -NoPackageAnalysis
-nuget pack WindowsAPICodePack-Shell.1.1.nuspec -NoPackageAnalysis
-nuget pack WindowsAPICodePack-ShellExtensions.1.1.nuspec -NoPackageAnalysis
+nuget pack WindowsAPICodePack-Core.nuspec -NoPackageAnalysis
+nuget pack WindowsAPICodePack-ExtendedLinguisticServices.nuspec -NoPackageAnalysis
+nuget pack WindowsAPICodePack-Sensors.nuspec -NoPackageAnalysis
+nuget pack WindowsAPICodePack-Shell.nuspec -NoPackageAnalysis
+nuget pack WindowsAPICodePack-ShellExtensions.nuspec -NoPackageAnalysis
+nuget pack WindowsAPICodePack-DirectX.nuspec -NoPackageAnalysis
+REM nuget pack WindowsAPICodePack-DirectX.1.1-x64.nuspec -NoPackageAnalysis
+REM nuget pack WindowsAPICodePack-DirectX.1.1-x86.nuspec -NoPackageAnalysis
